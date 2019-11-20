dd6abaf9098de8ee2893ae2a46c13b7d2909037f
ramitsuri <ramitsuri007@gmail.com>
2019-11-20 16:11:35 -0500

    Add getter and setter to SheetMetaData

--- sheetscore/src/main/java/com/ramitsuri/sheetscore/consumerResponse/SheetMetadata.java
+++ sheetscore/src/main/java/com/ramitsuri/sheetscore/consumerResponse/SheetMetadata.java
@@ -9,6 +9,22 @@ public class SheetMetadata {
         mSheetName = sheetName;
     }
 
+    public int getSheetId() {
+        return mSheetId;
+    }
+
+    public void setSheetId(int sheetId) {
+        mSheetId = sheetId;
+    }
+
+    public String getSheetName() {
+        return mSheetName;
+    }
+
+    public void setSheetName(String sheetName) {
+        mSheetName = sheetName;
+    }
+
     @Override
     public String toString() {
         return "SheetMetadata{" +
